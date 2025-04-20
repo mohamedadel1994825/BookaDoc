9df144cba1f4b61a1f154ae4dc4e1996f93c2079
Mohamed Adel <mohamedadel1994825@gmail.com>
2025-04-20 20:27:28 +0200

    remove un used files

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    userId: string; // Unique identifier for each user
-    firstName: string;
-    lastName?: string;
-    username: string;
-    password: string;
-    email: string;
-  }
-  
